44299a42a24fc31eafec9fdb03f3222a7ff96f22
Ramiogue <146667454+Ramiogue@users.noreply.github.com>
2025-03-13 23:59:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 import io
 
 # ✅ Load the best saved Prophet model from GitHub
-MODEL_URL = "https://raw.githubusercontent.com/Ramiogue/stock-prices-analysis-/main/best_prophet_model.pkl"
+MODEL_URL = "https://github.com/Ramiogue/stock-prices-analysis-/blob/60eb4f841b07da0d1b2bb85871275a5d89e5e861/best_prophet_model.pkl"
 
 @st.cache_data
 def load_model():
